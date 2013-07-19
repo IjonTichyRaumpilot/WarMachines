f19d7a5f6c84a37a777453c94834339151e7c2ce
JohnDoe85 <arthur.zamfir@gmail.com>
2013-07-19 16:41:32 +0200

    WarMachines

---
+++ The WarMachine Game/src/edu/WarMachineGame/WarMachines/Fregatte.java
@@ -0,0 +1,13 @@
+package edu.WarMachineGame.WarMachines;
+
+import edu.WarMachineGame.SpielRaum.Ausrichtung;
+import edu.WarMachineGame.SpielRaum.Koordinate;
+
+public class Fregatte extends WarMachine{
+
+	public Fregatte(Koordinate koord, Ausrichtung ausrichtung) {
+		super(koord, ausrichtung);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ The WarMachine Game/src/edu/WarMachineGame/WarMachines/Schlauchboot.java
@@ -0,0 +1,13 @@
+package edu.WarMachineGame.WarMachines;
+
+import edu.WarMachineGame.SpielRaum.Ausrichtung;
+import edu.WarMachineGame.SpielRaum.Koordinate;
+
+public class Schlauchboot extends WarMachine{
+
+	public Schlauchboot(Koordinate koord, Ausrichtung ausrichtung) {
+		super(koord, ausrichtung);
+		// TODO Auto-generated constructor stub
+	}
+
+}
